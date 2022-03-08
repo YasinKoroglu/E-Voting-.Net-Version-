a5e4914fb880dcaf3ada277da6c24c011fc067f7
Yasin Koroglu <yasin.koroglu@isik.edu.tr>
2022-03-08 12:58:28 +0300

    Online Voting System
    Developed with C# and .Net

---
+++ E-Voting(.Net Version)/E-Voting/Controllers/HomeController.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using E_Voting.Models;
+
+
+
+
+
+
+
+namespace E_Voting.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult ContactUs()
+        {
+
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Savecontactus(RespectewLetter model)
+        {
+
+            try
+            {
+                AccountdbEntities db = new AccountdbEntities();
+
+                RespectewLetter rgs = new RespectewLetter();
+                rgs.fullname = model.fullname;
+                rgs.givenemail = model.givenemail;
+                rgs.yourletter = model.yourletter;
+                
+
+                db.RespectewLetters.Add(rgs);
+
+            
+                
+            
+                db.SaveChanges();
+          
+
+
+            int latestElcId = rgs.Id;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+
+
+            return RedirectToAction("home");
+        }
+
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult LoginUser(User model)
+        {
+            AccountdbEntities db = new AccountdbEntities();
+            User user = db.Users.SingleOrDefault(x => x.email == model.email && x.password1 == model.password1);
+            if (user != null)
+            {
+                Session["UserId"] = user.Id;
+                Session["Username"] = user.firstname;
+
+            }
+
+
+            return Json("", JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult logout()
+        {
+            Session.Clear();
+            Session.Abandon();
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Saveuser(User model)
+        {
+
+            try
+            {
+                AccountdbEntities db = new AccountdbEntities();
+
+                User usr = new User();
+                usr.firstname = model.firstname;
+                usr.lastname = model.lastname;
+                usr.email = model.email;
+                usr.socialsecuritynumber = model.socialsecuritynumber;
+                usr.password1 = model.password1;
+                usr.password2 = model.password2;
+
+                db.Users.Add(usr);
+
+                db.SaveChanges();
+
+                int latestUsrId = usr.Id;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            
+
+
+               return Json("Success", JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult forgotpassword()
+        {
+            return View();
+        }
+
+        public ActionResult home()
+        {
+            return View();
+        }
+
+        public ActionResult createcampaign()
+        {
+
+           // AccountdbEntities db = new AccountdbEntities();
+            //List<Election> list = db.Elections.ToList();
+            //ViewBag.ElectionList = new SelectList(list, "Campaign_Name", "Participant_Name1", "Participant_Name2");
+            return View();
+
+        }
+
+        public ActionResult Savecampaign(Election model)
+        {
+
+            try
+            {
+                AccountdbEntities db = new AccountdbEntities();
+
+                Election elb = new Election();
+                elb.CampaignName = model.CampaignName;
+                elb.ParticipantName1 = model.ParticipantName1;
+                elb.ParticipantName2 = model.ParticipantName2;
+                elb.Content1 = model.Content1;
+
+                db.Elections.Add(elb);
+                
+                db.SaveChanges();
+
+                int latestCampaignId = elb.Id;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+
+
+            return RedirectToAction("profile");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public ActionResult setting()
+        {
+            return View();
+        }
+
+        
+        public ActionResult profile()
+        {
+            return View();
+        }
+
+        public ActionResult campaigns()
+        {
+             AccountdbEntities db = new AccountdbEntities();
+            var data = db.Elections.ToList();
+            ViewBag.userdetails = data;
+
+            return View();
+        }
+
+        public ActionResult Savevote(Vote model)
+        {
+            try
+            {
+                AccountdbEntities db = new AccountdbEntities();
+
+                Vote elb = new Vote();
+                elb.CampaignName = model.CampaignName;
+                elb.WhichParticipant = model.WhichParticipant;
+                elb.Whovotedname = model.Whovotedname;
+                elb.Whovotedemail = model.Whovotedemail;
+               //Session["Username"] = model.Whovotedname;
+               // Session["UserId"] = model.Whovotedemail;
+
+                db.Votes.Add(elb);
+
+
+                db.SaveChanges();
+
+                int latestVoteId = elb.Id;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+
+
+            return RedirectToAction("profile");
+        }
+
+
+
+        public JsonResult GetCampaignRecord()
+        {
+            AccountdbEntities db = new AccountdbEntities();
+            List<Election> List = db.Elections.Select(x => new Election 
+            { 
+                CampaignName = x.CampaignName,
+                Content1 = x.Content1,
+                ParticipantName1 = x.ParticipantName1,
+                ParticipantName2 = x.ParticipantName2 
+            }).ToList();
+            
+            return Json(List, JsonRequestBehavior.AllowGet);
+        }
+       
+
+        public ActionResult myCampaigns()
+        {
+            return View();
+        }
+    }
+}
---
+++ E-Voting(.Net Version)/E-Voting/signup.aspx
@@ -0,0 +1,90 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="signup.aspx.cs" Inherits="E_Voting.WebForm1" %>
+
+<!DOCTYPE html5>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SignUp</title>
+    <style>
+        input[type=button] {
+            background-color: #04AA6D;
+            color: white;
+            border: none;
+            margin-left: 10px;
+        }
+
+        input[type=text], input[type=button], input[type=password] {
+            width: 16%;
+            padding: 12px;
+            margin: 8px 0;
+            display: inline-block;
+            border: 1px solid #ccc;
+            box-sizing: border-box;
+            text-align: center;
+            margin-left: 20px;
+        }
+    </style>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="home">E-Voting</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="home">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="signup.aspx">Signup</a>
+        </li>
+          <li class="nav-item">
+          <a class="nav-link active" href="#">AboutUs</a>
+        </li>
+          <li class="nav-item">
+          <a class="nav-link active" href="home/ContactUs">ContactUs</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+    
+
+    <form id="form1" runat="server" method="post">
+         <div style="text-align: center; margin-top:75px">
+             <h2>SignUp</h2>
+            <input style="width: 300px" type="text" id="fname" name="fname" placeholder="First Name*" >
+            <br />
+            <input style="width: 300px" type="text" id="lname" name="lname" placeholder="Last Name*">
+            <br />
+            <input style="width: 300px" type="text" id="ssn" name="ssn" placeholder="SSN*">
+            <br />
+            <input style="width: 300px" type="text" id="email" name="email" placeholder="Email*">
+            <br />
+            <input style="width: 300px" type="password" id="password1" name="password1" placeholder="Password*">
+            <br />
+            <input style="width: 300px" type="password" id="password2" name="password2" placeholder="Password Again*">
+
+            <div>
+                <a href="Login">
+                    <input type="button" value="Login Instead" style=" " />
+                </a><br />
+                <a href="Login">
+                    <input type="button" value="SignUp" />
+                </a>
+            </div>
+        </div>
+        
+    </form>
+</body>
+</html>
